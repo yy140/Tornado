9e77d54d9192e021784c7fbaeb9b4ee27ad63051
HarryCarton <harrycarton@hotmail.com>
2021-03-29 16:55:36 +0100

    added moving platforms and jumps

--- public/js/game2.js
+++ public/js/game2.js
@@ -7,7 +7,7 @@ var config = {
     default: 'arcade',
     arcade: {
       gravity: { y: 400 },
-      debug: true
+      debug: false
     }
   },
   scene: {
@@ -39,6 +39,9 @@ var isPaused = false;
 var acid;
 var gameOver = false;
 var bird;
+var moving_platform_1;
+var moving_platform_2;
+var theta = 0;
 
 function preload() {
 
@@ -49,6 +52,8 @@ this.load.image('bg', '../assets/game_2/bg.png');
 this.load.image('buildings', '../assets/game_2/buildings.png');
 this.load.image('far_buildings', '../assets/game_2/far-buildings.png');
 this.load.image('foreground', '../assets/game_2/skill-foreground.png');
+this.load.image('platform-10', '../assets/game_2/platform-10w.png');
+this.load.image('platform-25', '../assets/game_2/platform-25w.png');
 this.load.image('platform-50', '../assets/game_2/platform-50w.png');
 this.load.image('platform-100', '../assets/game_2/platform-100w.png');
 this.load.image('platform-200', '../assets/game_2/platform-200w.png');
@@ -67,11 +72,11 @@ function create() {
   this.cameras.main.setBounds(0, 0, 5000, 600);
 
   bg = this.add.tileSprite(0,0, 0, 0, 'bg').setScale(8);
-  far_buildings = this.add.tileSprite(0,300, 1000, 1000, 'far_buildings').setScale(5);
-  buildings = this.add.tileSprite(0,300, 1000, 1000, 'buildings').setScale(4);
-  foreground = this.add.tileSprite(0,500, 1000, 100, 'foreground').setScale(3);
+  far_buildings = this.add.tileSprite(0,300, 5000, 1000, 'far_buildings').setScale(5);
+  buildings = this.add.tileSprite(0,300, 5000, 1000, 'buildings').setScale(4);
+  foreground = this.add.tileSprite(0,500, 5000, 100, 'foreground').setScale(3);
   
-  acid = this.add.tileSprite(0,560,5000,200, 'acid_1');
+  acid = this.add.tileSprite(0,560,10000,200, 'acid_1');
   
   this.physics.add.existing(acid, true);
   acid.body.setOffset(0,100)
@@ -101,6 +106,8 @@ function create() {
 
   trust_text = this.add.text(1400, 300, 'Just jump...trust me', { fontSize: '16px', fill: '#000'});
 
+  trust_text = this.add.text(1800, 300, 'Mind the gap', { fontSize: '16px', fill: '#000'});
+
   platforms = this.physics.add.staticGroup();
   platforms.create(0, 500, 'platform-100');
   platforms.create(200, 500, 'platform-100');
@@ -112,16 +119,31 @@ function create() {
   platforms.create(900, 275, 'platform-200');
   platforms.create(1150, 475, 'platform-50');
   platforms.create(1350, 500, 'platform-50');
+  platforms.create(1800, 500, 'platform-500').setVisible(false);
+  platforms.create(2400, 500, 'platform-500');
+  platforms.create(3900, 500, 'platform-25');
+  platforms.create(4100, 400, 'platform-25');
+  platforms.create(4050, 300, 'platform-25');
+  platforms.create(4150, 200, 'platform-50');
+
+  moving_platform_1 = this.physics.add.sprite(2900,300, 'platform-50').setImmovable(true);
+  moving_platform_1.body.setAllowGravity(false);
+  moving_platform_1.body.friction.y=100;
+  moving_platform_2 = this.physics.add.sprite(3300,300, 'platform-50').setImmovable(true);
+  moving_platform_2.body.setAllowGravity(false);
+ 
+  
 
   player = this.physics.add.sprite(25, 300, 'dude');
   player.setOrigin(0.5,0.5)
 
-  player.setBounce(0.01);
+  //player.setBounce(0.01);
   player.setCollideWorldBounds(true);
   cursors = this.input.keyboard.createCursorKeys();
 
-  bird = this.physics.add.sprite(200, 500, 'bird');
+  bird = this.physics.add.sprite(500, 500, 'bird');
   bird.body.setAllowGravity(false);
+  bird.setSize(24, 20).setOffset(8, 12);
 
   this.anims.create({ key: 'fly_right', 
     frames: this.anims.generateFrameNumbers('bird', {start:0, end:8}), 
@@ -168,6 +190,8 @@ function create() {
 
   this.cameras.main.startFollow(player, true);
   this.physics.add.collider(player, platforms);
+  this.physics.add.collider(player, moving_platform_1);
+  this.physics.add.collider(player, moving_platform_2);
   this.physics.add.collider(player, acid, player_die, null, this);
   this.physics.add.collider(player, bird, player_die, null, this);
   
@@ -176,6 +200,8 @@ function create() {
   }
 
   function update(){
+    theta+=0.01;
+    update_moving_platform()
     
     update_bird()
 
@@ -240,4 +266,10 @@ function update_bird(){
     bird.body.position.y =  bird.body.position.y + 0.006*(player.body.y - bird.body.position.y);
   }
 
+}
+function update_moving_platform(){
+  moving_platform_1.body.position.x += Math.cos(theta);
+  moving_platform_1.body.position.y += Math.sin(theta*2);
+  moving_platform_2.body.position.x += Math.cos(theta);
+  moving_platform_2.body.position.y += Math.sin(-theta*2);
 }
