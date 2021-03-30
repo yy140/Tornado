d0de67fff736c6c62758897758847d8bd7d114b4
yy140 <74052695+yy140@users.noreply.github.com>
2021-03-30 23:45:58 +0100

    playerWin broke something

--- public/js/game3.js
+++ public/js/game3.js
@@ -6,36 +6,32 @@ var config = {
   physics: {
     default: 'arcade',
     arcade: {
-      gravity: { y: 0 },
-      debug: false
+      debug: true
     }
   },
   scene: {
     preload: preload,
     create: create,
     update: update
-  },
-  scale: {
-    parent: 'myGame',
-    autoCenter: Phaser.Scale.CENTER_BOTH,
-    width: 720,
-    height: 720
-  }
+  } 
 };
 
 var game = new Phaser.Game(config);
 var player
 var coins
 var coinLayer
-
+var enemies
+var enemyLayer
+var gameOver = false
+var gameWin = false
 
 function preload() 
   {
     // load the PNG file
     this.load.image('tiles', '/assets/game3/rogue.png')
-  
+    this.load.image('star', '/assets/game3/star.png')
     // load the JSON file
-    this.load.tilemapTiledJSON('tilemap', '../assets/game3/levelOne.json' )
+    this.load.tilemapTiledJSON('tilemap', '/assets/game3/levelOne.json' )
     this.load.spritesheet('coin', '/assets/game3/coin.png', {
       frameWidth: 32,
       frameHeight: 32
@@ -44,44 +40,73 @@ function preload()
       frameWidth: 80,
       frameHeight: 110
     })
-     
-    
+    this.load.spritesheet('enemy', '/assets/game3/zombie.png', {
+      frameWidth: 80,
+      frameHeight: 110
+    }) 
   }
   
   function create()
   {
     
+
     const map = this.make.tilemap({key: 'tilemap',})
     const tileset = map.addTilesetImage('rogue', 'tiles')
     const background = map.createLayer('background', tileset, 0, 0).setScale(3)
     const blocked =  map.createLayer('blocked', tileset, 0, 0).setScale(3)
     const blockedaboveplayer =  map.createLayer('blockedaboveplayer', tileset, 0, 0).setScale(3)    
     coinLayer = map.getObjectLayer('coins')['objects']
-    //enemyLayer = map.getObjectLayer('enemies')['objects']
     blocked.setCollisionByProperty({ collides: true})
     blockedaboveplayer.setCollisionByProperty({ collides: true})
 
-    coins = this.physics.add.staticGroup()
 
+        coins = this.physics.add.staticGroup()
     coinLayer.forEach(object => {
       let obj = coins.create(object.x * 2.75, object.y * 3, 'coin'); 
          obj.setOrigin(0); 
          obj.body.width = object.width; 
          obj.body.height = object.height;
          obj.setSize(25,25).setOffset(18, 15)
-    
-      
     })
+    
+  enemies = this.add.group()
+  map
+  .filterObjects('enemies', (object) => object.type === 'enemy')
+  .forEach((enemy) => {
+    let enemySprite = this.physics.add.sprite(enemy.x * 2.75, enemy.y * 3, 'enemy')
+    enemySprite.body.setImmovable(true)
+    enemySprite.setCollideWorldBounds(true)
+    this.physics.add.collider(enemySprite, blocked)
+    this.physics.add.collider(enemySprite, blockedaboveplayer)
+    enemySprite.setVelocityX(getRandomNumber() * 1.25 )
+    enemySprite.setVelocityY(getRandomNumber() * 1.25)
+        enemies.add(enemySprite)
+  })
+enemies.enableBody = true;
+enemies.getChildren().forEach((enemy) => enemy.setScale(0.45).setSize(60, 90).setOffset(0,25))
 
-     //enemies = this.physics.add.group();
-    // enemies.enableBody = true;
+this.add.image(650, 120, 'star');
+    
 
-    // enemyLayer.forEach(object => {
-    //   let obj = enemies.create(object.x * 2.75, object.y * 3, 'enemy'); 
-    //      obj.setOrigin(0); 
-    //      obj.body.width = object.width; 
-    //      obj.body.height = object.height;
-    //      obj.setSize(10,25).setOffset(18, 15)})
+gameOverText = this.add.text(400, 300, "         Game Over\n Click here to try again!", { fontSize: '32px', fill: '#000'});
+gameOverText.setOrigin(0.5).setInteractive();
+gameOverText.visible = false
+gameOverText.on('pointerdown', () => { 
+  gameOver = false;
+  this.registry.destroy(); // destroy registry
+  this.events.off(); // disable all active events
+  this.scene.restart(); // restart current scen
+});
+gameWinText = this.add.text(400, 300, "  You Win! Congratulations\n Click here to try again!", { fontSize: '32px', fill: '#000'});
+gameWinText.setOrigin(0.5).setInteractive();
+gameWinText.visible = false
+gameWinText.on('pointerdown', () => { 
+  gameWin = false;
+  this.registry.destroy(); // destroy registry
+  this.events.off(); // disable all active events
+  this.scene.restart(); // restart current scen
+});
+
 
        this.anims.create({
           key: 'spin',
@@ -90,8 +115,7 @@ function preload()
           repeat: -1
         });
             
-
-        player = this.physics.add.sprite(100, 680, 'boy').setScale(0.4);
+        player = this.physics.add.sprite(100, 680, 'boy').setScale(0.4).setSize(50,100);
         this.anims.create({
         key: 'left',
         frames: this.anims.generateFrameNumbers('boy', { start: 0, end: 3 }),
@@ -102,7 +126,7 @@ function preload()
       this.anims.create({
         key: 'turn',
         frames: [ { key: 'boy', frame: 23 } ],
-        frameRate: 20
+        frameRate: 0
       });
     
       this.anims.create({
@@ -125,13 +149,14 @@ function preload()
       });
       cursors = this.input.keyboard.createCursorKeys();
       
+      
       player.setCollideWorldBounds(true)
       this.physics.add.collider(player, blocked)
       this.physics.add.collider(player, blockedaboveplayer)
-      //this.physics.add.collider(player, coinLayer)
-      //this.physics.add.collider(player, enemies)
+      //this.physics.add.collider(player, enemies, enemyStrike, null, this)
       this.physics.add.overlap(player, coins, collectCoin, null, this)
-
+      
+    
       //add a score
       // text = this.add.text(570, 70, `Coins: ${coinScore}x`, {
       //   fontSize: '20px',
@@ -139,48 +164,90 @@ function preload()
       // });
       // text.setScrollFactor(0);
       
+      this.anims.create({
+        key:'walk',
+        frames: this.anims.generateFrameNumbers('enemy', { start: 11, end: 12 }),
+        frameRate: 2,
+        repeat: -1
+      });
 
   }
 
-  function update()
+function update()
 {
+  playerWin()
+
   if (cursors.left.isDown){
     player.setVelocityX(-160);
  
     player.anims.play('left', true);
   }
-  else if (cursors.right.isDown){
+ if (cursors.right.isDown){
     player.setVelocityX(160);
 
     player.anims.play('right', true);
   } 
-  else if (cursors.down.isDown){
+ if (cursors.down.isDown){
     player.setVelocityY(160);
 
     player.anims.play('down', true);
   }
-  else if (cursors.up.isDown){
+ if (cursors.up.isDown){
     player.setVelocityY(-160);
 
     player.anims.play('up', true);
   }
-  else {
-    player.setVelocity(0);
+  // else {
+  //   player.setVelocity(0);
 
-    player.anims.play('turn');
-  }
+  //   player.anims.play('turn');
+  // }
 
-  if (cursors.up.isDown && player.body.touching.down){
-    player.setVelocityY(-330);
-  }
-
+  
   coins.getChildren().forEach((coin) => coin.anims.play( 'spin', true))
+  
+   enemies.getChildren().forEach((enemy) => { enemy.anims.play( 'walk', true);
+   })
 
+   moveZombies()
 }
+
+
 
 function collectCoin(player, coin) {
   coin.destroy(coin.x, coin.y); // remove the tile/coin
-  // coinScore ++; // increment the score
-  // text.setText(`Coins: ${coinScore}x`); // set the text to show the current score
-  return false;
+  //  coinScore ++; // increment the score
+  //  text.setText(`Coins: ${coinScore}x`); // set the text to show the current score
+  // return false
+
 }
+
+function enemyStrike() {
+  this.physics.pause();
+  player.setTint(0xff0000);
+  player.anims.play('turn');
+  gameOver = true;
+  gameOverText.visible = true;
+}
+
+function moveZombies() {
+  enemies.getChildren().forEach((enemy) => { 
+  enemy.setVelocityX(enemy.body.velocity.x + getRandomNumber())
+  enemy.setVelocityY(enemy.body.velocity.y + getRandomNumber())
+});
+}
+
+// function playerWin() {
+//   if (player.body.position.x > 100 && player.body.position.y > 1 ) { 
+//     console.log(this.player)
+// //this.enemies.physics.pause()
+// enemies.physics.pause()
+//     player.anims.play('turn');
+//     gameWin = true;
+//     gameWinText.visible = true; }
+// }
+
+function getRandomNumber() {
+  return (Math.random() - 0.5) * 5
+}
+   
