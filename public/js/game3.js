8f68d2eb47e77c01ab5f8f27756e46439734a946
yy140 <74052695+yy140@users.noreply.github.com>
2021-03-31 15:19:34 +0100

    delete password from cookie

--- public/js/game3.js
+++ public/js/game3.js
@@ -2,8 +2,7 @@ var config = {
   type: Phaser.AUTO,
   // parent: 'phaser-example',
   width: 720,
-  height: 720,
-  physics: {
+  height: 720cc  physics: {
     default: 'arcade',
     arcade: {
       debug: false
